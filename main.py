66017da3a229e2d33a31b8c8d563af8a4f36ec1d
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-12-29 17:39:53 +0330

    rsi

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import schedule
 import time
 from tradingview_ta import TA_Handler, Exchange, Interval
-import matplotlib.pyplot as plt
 
-plt.ion()  # Turn on interactive mode
-
-rsi_values = []  # List of RSI values
-timestamps = []  # List of timestamps
-
-fig, ax = plt.subplots()
-
-def printRSI():
+def getData():
     handler = TA_Handler(
     symbol='BTCUSDT',
     screener='crypto',
@@ -18,32 +10,38 @@ def printRSI():
     interval=Interval.INTERVAL_1_MINUTE
     )
     analysis = handler.get_analysis()
-    rsi = analysis.indicators['RSI[1]']
-    moving_average= analysis.indicators    
-    if rsi < 30 :
-        print (rsi , 'Buy')
-    elif rsi > 60 :
-        print ( rsi , 'Sell') 
-    else : print(rsi , "Not signal")       
-
-    rsi_values.append(rsi)  # Append RSI value to the list
-    timestamps.append(time.time())  # Append current time to the list
-
-    # Plot the chart
-    ax.clear()  # Clear the previous chart
-    ax.plot(timestamps, rsi_values, label='RSI')
-    ax.set_xlabel('TIME')
-    ax.set_ylabel('Volume RSI')
-    ax.set_title('Chart RSI')
-    ax.legend()
-    plt.pause(0.01)  # Pause to update the chart
+    RSI = analysis.indicators['RSI[1]']
+    if RSI > 60 :
+        print(RSI)
+    
 
 # Schedule the task to run every 5 seconds
-schedule.every(5).seconds.do(printRSI)
+schedule.every(5).seconds.do(getData)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # # Import packages.
 # from tradingview_ta import TA_Handler, Interval, Exchange
